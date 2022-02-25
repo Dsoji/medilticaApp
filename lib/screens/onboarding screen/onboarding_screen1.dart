3c3c7ed1e07d82e2c3731746abded32cba5edba2
Olasoji Oluwadamilola <dami.osoj@gmail.com>
2022-02-25 17:19:39 +0100

    Second commit

--- lib/screens/onboarding screen/onboarding_screen1.dart
+++ lib/screens/onboarding screen/onboarding_screen1.dart
@@ -14,8 +14,12 @@ class Onboardingcreen1 extends StatelessWidget {
             child: Column(
               children: [
                 Padding(
-                  padding: EdgeInsets.all(30),
-                  child: Image.asset(''),
+                  padding: const EdgeInsets.all(1.0),
+                  child: Image.asset(
+                    'assets/images/Images.png',
+                    height: 550,
+                    width: double.infinity,
+                  ),
                 ),
                 Center(
                   child: Text(
@@ -31,12 +35,74 @@ class Onboardingcreen1 extends StatelessWidget {
           ),
           Container(
             color: mBackgroundColor1,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image.asset(
+                      'assets/images/illustrations.png',
+                      height: 300,
+                      //width: 700,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Unlock Better Health',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
           Container(
             color: mBackgroundColor1,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Image.asset(
+                      'assets/images/Health tips.png',
+                      height: 400,
+                      width: 700,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                  Center(
+                    child: Text(
+                      'Unlock Better Health',
+                      style: TextStyle(
+                        fontSize: 35,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
           Container(
             color: mBackgroundColor1,
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'assets/images/Group.png',
+                    height: 700,
+                    width: 700,
+                  ),
+                ],
+              ),
+            ),
           ),
         ],
       ),
